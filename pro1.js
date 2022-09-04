314fc380683afcf4786f8389708495e884ffb2b0
nisha027 <nishaguptag399@gmail.com>
2022-09-04 12:26:43 +0530

    First commit

---
+++ pro1.js
@@ -0,0 +1,19 @@
+// Problem 1: Given the year of birth, if the age is between 13 and 19 (both included) print Teenage and in between 20 and 29 (both included) print Twenties
+function teenageOrTwenty(age,birthyear){
+ 
+ if(age>=13&&age<=19){
+   console.log("Teenage");
+ }
+  else if(age>=20&&age<=29){
+    console.log("Twenties")
+  }
+  else{
+   console.log("NA");
+  }
+  
+}
+let birthyear=2000;
+let age=2022-birthyear
+
+teenageOrTwenty(age,birthyear);
+
---
+++ pro2.js
@@ -0,0 +1,1 @@
+// Problem 2 : Given any character, if it is a vowel print "Vowel"
